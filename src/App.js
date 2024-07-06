7427aeb3b388e6c6b57336b81a1c7da1d6603951
Abdullah <ishtiaqabdullah11@gmail.com>
2024-07-06 01:41:42 -0600

    changed background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Introduction from './components/Introduction';
 import Projects from './components/Projects';
-import ThreeBackground from './ThreeBackground';
 import './App.css';
 
 const App = () => {
   return (
     <div id="app">
-      <ThreeBackground />
+      <video id="background-video" autoPlay loop muted>
+        <source src={`${process.env.PUBLIC_URL}/background.mp4`} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div style={{ position: 'relative', zIndex: 1 }}>
         <Introduction />
         <Projects />
