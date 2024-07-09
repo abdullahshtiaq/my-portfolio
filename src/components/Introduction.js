52c69c489d6f30cf9b16a05e39280aec0652ce67
Abdullah <ishtiaqabdullah11@gmail.com>
2024-07-08 22:42:40 -0600

    trimmed video

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -25,20 +25,6 @@ const Introduction = () => {
           <div className="dropdown">
             <button>
               <span>Projects</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="w-5 h-5 ml-2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
             </button>
             <div className="dropdown-content">
               <ul>
